192b891b19e5e45ddb5c0dc2d090899f8e5dbe6c
Tanbir Alam <itzmetanbir@gmail.com>
2023-11-13 11:11:16 +0530

    Navbar and Header section updated

---
+++ src/container/About/About.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import  { motion } from "framer-motion"
+
+import './About.scss'
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
