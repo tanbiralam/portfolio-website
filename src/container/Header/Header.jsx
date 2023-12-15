1a3b4f6052ba5a14e985ea247daefab255a2e8dc
Tanbir Alam <itzmetanbir@gmail.com>
2023-12-15 13:19:19 +0530

    some modifications

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -34,8 +34,7 @@ const Header = () => {
           </div>
 
           <div className="tag-cmp app__flex">
-            <p className="p-text">Web Developer</p>
-            <p className="p-text">Freelancer</p>
+            <p className="p-text">a programmer, a designer and an artist</p>
           </div>
         </div>
       </motion.div>
@@ -61,7 +60,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-       {[images.flutter, images.redux, images.sass].map((circle, index) => (
+       {[images.react, images.javascript, images.git].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="circle" />
         </div>
